c51bcbd1a2e478aaa5aaf7d7e85ab940d8c2299d
jonathangonzalezlpz <jonathan.gonzalezlpz@gmail.com>
2021-01-30 17:24:19 +0100

    Fin sección 3 rutas establecidas y hash

---
+++ src/app/app-routing.modulo.ts
@@ -0,0 +1,31 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProductoComponent } from './pages/producto/producto.component';
+
+const app_routes: Routes = [
+    { path: 'home', component: PortafolioComponent}, //cambiamos ''vacío por home para evitar pequeños bugss relacionados con routerlinkActive
+    { path: 'about', component: AboutComponent},
+    { path: 'producto', component: ProductoComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'home'} //Cualquier cosa distinta de las routas indicadas redirige a portafolio 
+];
+
+@NgModule({
+    imports: [ //Como RouterModule es un módulo utilizamos imports
+        RouterModule.forRoot( app_routes, { useHash: true}) //useHash:true, incluye un # en la ruta para indicar que lo que viene después no 
+        //es un directorio, sino una parte de la ruta del index.html que se encuentra en esa dirección
+    ],
+    exports: [ //Hay que exportar para que pueda ser utilizado fuera, en este caso en el app-component
+        RouterModule
+    ]
+}) 
+
+export class AppRoutingModule{
+
+}
+
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+//Rutas
+import { AppRoutingModule } from './app-routing.modulo';
 
+//Componentes
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProductoComponent } from './pages/producto/producto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    AboutComponent,
+    ProductoComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [ //La parte de los módulos por lo general va aquí
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
