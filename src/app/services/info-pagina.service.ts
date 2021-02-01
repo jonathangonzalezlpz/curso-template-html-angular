6dba19048e5d979d0ffbc0497011aa8d85e03dff
jonathangonzalezlpz <jonathan.gonzalezlpz@gmail.com>
2021-02-01 14:51:24 +0100

    Fin Sección 5 equipo cogido de Firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,13 +8,25 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
 export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[]=[];
+
   constructor(private http: HttpClient) { //servicio incluido en el modulo importado en appmodule HttpClientModule
     //Este modulo nos permite conectarnos por ejemplo a servidores REST
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo(){
     //Leer el archivo JSON
     this.http.get('assets/data/data-pagina.json').subscribe((resp: InfoPagina)=> {
       this.cargada=true;
       this.info=resp;
-      //console.log(resp['twitter']);
+    });
+  }
+
+  private cargarEquipo(){
+    this.http.get('https://angular-html-c8ab4-default-rtdb.firebaseio.com/equipo.json').subscribe((resp: any[])=> {
+      this.equipo=resp;
     });
   }
 }
